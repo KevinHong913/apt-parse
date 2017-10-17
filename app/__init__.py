ab5c7789a1b29f3f7d1059c5d9454eb3aeb6a183
KevinHong <hungwe55@gmail.com>
2017-10-17 00:32:47 -0700

    datastore work

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
-# Import flask and template operators
-from flask import Flask, render_template
+# [START app]
+
+
+# [START imports]
+import logging
+from flask import Flask, render_template, request
+from google.appengine.ext import ndb
+# [END imports]
 
 # Import SQLAlchemy
-from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy import SQLAlchemy
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -10,23 +16,54 @@ app = Flask(__name__)
 # Configurations
 app.config.from_object('config')
 
-# Define the database object which is imported
-# by modules and controllers
-db = SQLAlchemy(app)
 
-# Sample HTTP error handling
-# @app.errorhandler(404)
-# def not_found(error):
-#     return render_template('404.html'), 404
+# [START form]
+@app.route('/form')
+def form():
+    return render_template('form.html')
+# [END form]
 
-# Import a module / component using its blueprint handler variable (mod_auth)
-# from app.mod_auth.controllers import mod_auth as auth_module
 
-# Register blueprint(s)
-# app.register_blueprint(auth_module)
-# app.register_blueprint(xyz_module)
-# ..
+# [START submitted]
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
 
-# Build the database:
-# This will create the database file using SQLAlchemy
-# db.create_all()
+    # [END submitted]
+    # [START render_template]
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
+    # [END render_template]
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+
+
+class Task(ndb.Model):
+    description = ndb.StringProperty()
+    done = ndb.BooleanProperty()
+
+
+@app.route('/datastore')
+def run_datastore():
+    # [START datastore_quickstart]
+    # Imports the Google Cloud client library
+
+    task = Task(
+        description='Sandy', done=False)
+    task.put()
+
+    return render_template('form.html')
+# if __name__ == '__main__':
+#   run_datastore()
